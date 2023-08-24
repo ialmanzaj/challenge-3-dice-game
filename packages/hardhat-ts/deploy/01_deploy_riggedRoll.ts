26be7f02d7da7bcffa7873623e1ea190c1ecbe15
Isaac Almanza <isaac.almanza19@gmail.com>
2023-08-24 09:44:53 -0500

    feat: add contract with rugged dice

--- packages/hardhat-ts/deploy/01_deploy_riggedRoll.ts
+++ packages/hardhat-ts/deploy/01_deploy_riggedRoll.ts
@@ -10,19 +10,18 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironmentExtended) => {
 
     const DiceGameContract = await hre.deployments.get('DiceGame');
 
-    /*
+
     await deploy('RiggedRoll', {
         // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
         from: deployer,
         args: [DiceGameContract.address],
         log: true,
     });
-    */
 
     // Getting a previously deployed contract
-    //const riggedRoll = await ethers.getContract("RiggedRoll", deployer);
+    const riggedRoll = await ethers.getContract("RiggedRoll", deployer);
 
-    //const ownershipTransaction = await riggedRoll.transferOwnership("** YOUR FRONTEND ADDRESS **");
+    const ownershipTransaction = await riggedRoll.transferOwnership("0x10703A1205401A0947E2FA4331A2FE48D49570a8");
 
 };
 export default func;
